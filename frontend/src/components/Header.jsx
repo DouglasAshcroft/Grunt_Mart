04334e00d7d85a0c38010514658778460cf655ae
jake <jake.glee1122@gmail.com>
2025-08-22 12:35:04 -0700

    started components

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <div>
+      <h1>Grunt Mart</h1>
+      <div>Shopy by category</div>
+      <form>Search</form>
+      <div>shopping cart</div>
+    </div>
+  );
+}
---
+++ frontend/src/components/PopularItems.jsx
@@ -0,0 +1,3 @@
+export default function PopularItems() {
+  return <div></div>;
+}
