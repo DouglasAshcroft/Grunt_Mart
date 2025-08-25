fbd65938e6f84378f6f748d4d2ab1c7d8646cf66
jake <jake.glee1122@gmail.com>
2025-08-25 13:19:57 -0700

    test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,24 @@
 import "./App.css";
 import Header from "./components/Header.jsx";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { useState, useEffect, useContext } from "react";
+import AppContext from "./AppContext";
+import { CartContext } from "./context/CartContext.js";
 
 function App() {
+  const [shoppingCart, setShoppingCart] = useState([]);
+
   return (
-    <AppContext.Provider>
+    <CartContext.Provider value={{ shoppingCart, setShoppingCart }}>
       <Header />
 
       <Router>
         <Routes>
-          <Route path="/" />
+          <Route path="/" element={<Homepage />} />
+          <Route path="/view" element={<Viewbox />} />
         </Routes>
       </Router>
-    </AppContext.Provider>
+    </CartContext.Provider>
   );
 }
 export default App;
--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,3 +1,11 @@
 export default function About() {
-  return <div></div>;
+  return (
+    <div>
+      Grunt Mart is the one stop shop that provides all essential combat
+      equipment that is required for operational readiness. With years of
+      experience and knowledge of current and former Special Operations
+      Soldiers, Grunt Mart provides equipment that will be necessary to complete
+      the mission.{" "}
+    </div>
+  );
 }
