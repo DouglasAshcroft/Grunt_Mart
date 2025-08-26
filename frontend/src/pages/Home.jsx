5b2e11e838da0a23b869f434ab6d5b45b9fb6d41
jake <jake.glee1122@gmail.com>
2025-08-26 09:58:55 -0700

    jake

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,12 +3,13 @@ import Navbar from "../components/Navbar";
 import Viewbox from "../components/Viewbox";
 import Carousel from "../components/Carousel";
 import About from "../components/About";
+import { slides } from "../data/carouselData";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
-      <Carousel />
+      <Carousel data={slides} />
       <Viewbox />
       <About />
     </main>
