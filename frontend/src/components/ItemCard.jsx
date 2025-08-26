4c742a5c2b30154ba10d2ff9f10e4f99c60efc78
jake <jake.glee1122@gmail.com>
2025-08-26 13:06:54 -0700

    itemcards added, some styling added

--- frontend/src/components/ItemCard.jsx
+++ frontend/src/components/ItemCard.jsx
@@ -1,15 +1,28 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import "../styles/itemcard.css";
 
-export default function ItemCard() {
+export default function ItemCard({ product }) {
+  if (!product) return null;
   return (
     <div className="item-card">
-      <Link to="details" />
-      <div>image</div>
-      <h2>item name</h2>
-      <p>Description</p>
+      <Link to={`details/${product.product_id}`} />
+      <h2>{product.product_name}</h2>
+      <div>
+        {product.picture ? (
+          <img
+            className="picture-resize"
+            src={product.picture}
+            alt={product.name}
+          />
+        ) : (
+          "image"
+        )}
+      </div>
+      <h2>{product.name}</h2>
+
       <span>
-        <p>cost</p>
+        <p>{product.price}</p>
         <button>Add to cart</button>
       </span>
     </div>
--- frontend/src/context/SearchContext.jsx
+++ frontend/src/context/SearchContext.jsx
@@ -1,45 +1,45 @@
-// // src/context/SearchContext.jsx
-// import { createContext, useState, useMemo, useContext } from "react";
-// import Fuse from "fuse.js";
-// // import { APIContext } from "../components/ApiFetch"; Create the DB fetch to search
+// src/context/SearchContext.jsx
+import { createContext, useState, useMemo, useContext } from "react";
+import Fuse from "fuse.js";
+// import { APIContext } from "../components/ApiFetch"; Create the DB fetch to search
 
-// export const SearchContext = createContext();
+export const SearchContext = createContext();
 
-// export function SearchProvider({ children }) {
-//   const { products } = useContext(/*APIContext*/);
+export function SearchProvider({ children }) {
+  const { products } = useContext(/*APIContext*/);
 
-//   // Later: merge with venues & artists arrays
-//   const searchableData = useMemo(() => {
-//     return products.map((product) => ({
-//       id: "item",
-//       name: product.name,
-//       category: product.category?.name,
-//       role: product.role,
-//       // slug: event.venue ? event.venue.name : event.title,
-//     }));
-//   }, [products]);
+  // Later: merge with venues & artists arrays
+  const searchableData = useMemo(() => {
+    return products.map((product) => ({
+      id: "item",
+      name: product.name,
+      category: product.category?.name,
+      role: product.role,
+      // slug: event.venue ? event.venue.name : event.title,
+    }));
+  }, [products]);
 
-//   const fuse = useMemo(() => {
-//     return new Fuse(searchableData, {
-//       keys: ["name", "category", "role"],
-//       threshold: 0.3, // lower = stricter match
-//     });
-//   }, [searchableData]);
+  const fuse = useMemo(() => {
+    return new Fuse(searchableData, {
+      keys: ["name", "category", "role"],
+      threshold: 0.3, // lower = stricter match
+    });
+  }, [searchableData]);
 
-//   const [results, setResults] = useState([]);
+  const [results, setResults] = useState([]);
 
-//   const search = (query) => {
-//     if (!query.trim()) {
-//       setResults([]);
-//       return;
-//     }
-//     const matches = fuse.search(query).map((res) => res.item);
-//     setResults(matches);
-//   };
+  const search = (query) => {
+    if (!query.trim()) {
+      setResults([]);
+      return;
+    }
+    const matches = fuse.search(query).map((res) => res.item);
+    setResults(matches);
+  };
 
-//   return (
-//     <SearchContext.Provider value={{ results, search }}>
-//       {children}
-//     </SearchContext.Provider>
-//   );
-// }
+  return (
+    <SearchContext.Provider value={{ results, search }}>
+      {children}
+    </SearchContext.Provider>
+  );
+}
