54e2cfeac53ddb6bb989215cde93af72c5771972
mullity <mullis.tyler.work@gmail.com>
2025-08-28 09:09:14 -0600

    links working

--- frontend/src/components/ShoppingCart.jsx
+++ frontend/src/components/ShoppingCart.jsx
@@ -1,20 +1,25 @@
 import { useState, useContext } from "react";
 import { CartContext } from "../context/CartContext";
 import { useEffect } from "react";
-import { loadObjFromStorage, saveObjToStorage  } from "./utils/utils";
+import { loadObjFromStorage, saveObjToStorage } from "./utils/utils";
 import { toTitleCase, formatCurrency } from "./utils/reactUtils";
+import { Link, useNavigate } from "react-router-dom";
 import "../styles/ShoppingCart.css";
 
 export default function shoppingCart() {
     const { shoppingCart, setShoppingCart } = useContext(CartContext);
+    const navigate = useNavigate();
     console.log('shopping cart', shoppingCart)
 
     let currencyHandler = formatCurrency(`USD`)
 
-    function dumpShoppingCart(){
+    const handleNavigation = () => {;};
+
+    function dumpShoppingCart() {
         saveObjToStorage('browserShoppingCart', [])
         setShoppingCart([])
     }
+
     function checkCart() {
         if (!shoppingCart || shoppingCart.length < 1) {
             return (<div>No items in your cart! Get to shopping!</div>)
@@ -29,34 +34,42 @@ export default function shoppingCart() {
                 let product = cartItem['item']
                 return (
                     <li key={index} className="cartLineItem">
+                            {product.picture ? (
+                                <img
+                                    className="cartLineItemThumb"
+                                    src={product.picture}
+                                    alt={product.product_name}
+                                    onClick={()=>navigate(`../details/${product.product_id}`)}
+                                    style={{ cursor: 'pointer' }}
+                                />
+                            ) : (
+                                "image"
+                            )}
+                            <span 
+                            className="cartLineItemName" 
+                            
+                            onClick={()=>navigate(`../details/${product.product_id}`)} 
+                            style={{ cursor: 'pointer' ,color: 'var(--link)'}}>
+                                {toTitleCase(product.product_name)}
+                            </span>
 
-                        {product.picture ? (
-                            <img
-                                className="cartLineItemThumb"
-                                src={product.picture}
-                                alt={product.product_name}
-                            />
-                        ) : (
-                            "image"
-                        )}
-                        <span className="cartLineItemName">{toTitleCase(product.product_name)}</span>
                         <span>Unit Price: {currencyHandler.format(product.price)}</span>
                         <span>Quantity: {cartItem['qty']}</span>
                         <span>Subtotal: {currencyHandler.format(cartItem['qty'] * product.price)}</span>
                     </li>
                 )
-                
+
             })
-            let grandTotal = workingCart.reduce(((accumulator,currentItem)=>
-                accumulator+(currentItem['qty']*currentItem['item']['price'])),0)
+            let grandTotal = workingCart.reduce(((accumulator, currentItem) =>
+                accumulator + (currentItem['qty'] * currentItem['item']['price'])), 0)
 
             return (
                 <div className="cartContainer">
                     <ol className="cartList">{elementHolder}</ol>
                     <footer className="cartBottom">
-                        <button className="clearCartButton" onClick={()=>dumpShoppingCart()}>Empty Cart</button>
+                        <button className="clearCartButton" onClick={() => dumpShoppingCart()}>Empty Cart</button>
                         <span className="cartTotal">{currencyHandler.format(grandTotal)}</span>
-                        <button className="purchaseButton" onClick={()=>console.log(`Checked out $ ${grandTotal}`)}>Checkout</button>
+                        <button className="purchaseButton" onClick={() => console.log(`Checked out $ ${grandTotal}`)}>Checkout</button>
                     </footer>
                 </div>
 
